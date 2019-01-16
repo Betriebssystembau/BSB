b356db1ade8e1e8c2dad29c7fcdceb0365633ece
hkalvelage <hanneskalvelage@gmail.com>
2019-01-16 12:48:17 +0100

    Ausgabe in o_stream fertig

--- object/o_stream.cc
+++ object/o_stream.cc
@@ -30,139 +30,23 @@ O_Stream &O_Stream::operator<<(char c) {
 }
 
 O_Stream &O_Stream::operator<<(unsigned short number) {
-    char ascii_start = '0';
-    char ascii_alpha_start = 'a';
-    if (base == 2) {
-        *this << "0b";
-    } else if (base == 8) {
-        *this << "0o";
-    } else if (base == 16) {
-        *this << "0x";
-    }
-    if (number == 0) {
-        this->put(ascii_start);
-        return *this;
-    }
-    unsigned short div = 1;
-    while (div <= number / this->base) {
-        div *= this->base;
-    }
-
-
-    while (number > 0) {
-        if (number / div > 10) {
-            this->put((number / div) + ascii_alpha_start);
-        } else {
-            this->put((number / div) + ascii_start);
-        }
-        number %= div;
-        div /= this->base;
-    }
-    return *this;
+    unsigned long temp = (unsigned long) number;
+    return this->operator<<(temp);
 }
 
 O_Stream &O_Stream::operator<<(short number) {
-    char ascii_start = '0';
-    char ascii_alpha_start = 'a';
-    if (base == 2) {
-        *this << "0b";
-    } else if (base == 8) {
-        *this << "0o";
-    } else if (base == 16) {
-        *this << "0x";
-    }
-    if (number == 0) {
-        this->put(ascii_start);
-        return *this;
-    }
-    if (number < 0 && base == 10) {
-        this->put('-');
-        number *= -1;
-    }
-    short div = 1;
-    while (div <= number / this->base) {
-        div *= this->base;
-    }
-
-    while (number > 0) {
-        if (number / div > 10) {
-            this->put((number / div) + ascii_alpha_start);
-        } else {
-            this->put((number / div) + ascii_start);
-        }
-        number %= div;
-        div /= this->base;
-    }
-    return *this;
+    long temp = (long) number;
+    return this->operator<<(temp);
 }
 
 O_Stream &O_Stream::operator<<(unsigned int number) {
-    char ascii_start = '0';
-    char ascii_alpha_start = 'a';
-    if (base == 2) {
-        *this << "0b";
-    } else if (base == 8) {
-        *this << "0o";
-    } else if (base == 16) {
-        *this << "0x";
-    }
-    if (number == 0) {
-        this->put(ascii_start);
-        return *this;
-    }
-    unsigned int div = 1;
-    while (div <= number / this->base) {
-        div *= this->base;
-    }
-
-    while (number > 0) {
-        if (number / div > 10) {
-            this->put((number / div) + ascii_alpha_start);
-        } else {
-            this->put((number / div) + ascii_start);
-        }
-        number %= div;
-        div /= this->base;
-    }
-    return *this;
+    unsigned long temp = (unsigned long) number;
+    return this->operator<<(temp);
 }
 
 O_Stream &O_Stream::operator<<(int number) {
-    char ascii_start = '0';
-    char ascii_alpha_start = 'a';
-    if (base == 2) {
-        *this << "0b";
-    } else if (base == 8) {
-        *this << "0o";
-    } else if (base == 16) {
-        *this << "0x";
-    }
-    if (number == 0) {
-        this->put(ascii_start);
-        return *this;
-    }
-    if (number < 0 && base == 10) {
-        this->put('-');
-        number *= -1;
-    }
-    int div = 1;
-    while (div <= number / this->base) {
-        div *= this->base;
-    }
-
-
-    while (div > 0) {
-        if (number / div > 10) {
-            this->put((number / div) + ascii_alpha_start);
-        } else {
-            this->put((number / div) + ascii_start);
-        }
-
-        number %= div;
-
-        div /= this->base;
-    }
-    return *this;
+    long temp = (long) number;
+    return this->operator<<(temp);
 }
 
 O_Stream &O_Stream::operator<<(unsigned long number) {
@@ -184,7 +68,7 @@ O_Stream &O_Stream::operator<<(unsigned long number) {
         div *= this->base;
     }
 
-    while (number > 0) {
+    while (div > 0) {
         if (number / div > 10) {
             this->put((number / div) + ascii_alpha_start);
         } else {
@@ -210,7 +94,7 @@ O_Stream &O_Stream::operator<<(long number) {
         this->put(ascii_start);
         return *this;
     }
-    if (number < 0 && base == 10) {
+    if (number < 0) {
         this->put('-');
         number *= -1;
     }
@@ -219,7 +103,7 @@ O_Stream &O_Stream::operator<<(long number) {
         div *= this->base;
     }
 
-    while (number > 0) {
+    while (div > 0) {
         if (number / div > 10) {
             this->put((number / div) + ascii_alpha_start);
         } else {
@@ -232,28 +116,11 @@ O_Stream &O_Stream::operator<<(long number) {
 }
 
 O_Stream &O_Stream::operator<<(void *pointer) {
-    char ascii_start = '0';
-    char ascii_alpha_start = 'a';
-    *this << "0x";
-    unsigned int number = *(unsigned int *) pointer;
-    if (number == 0) {
-        this->put(ascii_start);
-        return *this;
-    }
-    long long div = 1;
-    while (div <= number / this->base) {
-        div *= 16;
-    }
-
-    while (number > 0) {
-        if (number / div > 10) {
-            this->put((number / div) + ascii_alpha_start);
-        } else {
-            this->put((number / div) + ascii_start);
-        }
-        number %= div;
-        div /= 16;
-    }
+    short b = this->base;
+    this->base = 16;
+    unsigned long number = *(unsigned long *) pointer;
+    this->operator<<(number);
+    this->base = b;
     return *this;
 }
 
